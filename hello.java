a2ace4823ee4a3109cb23b1da39c51cf39113869
athikeshav <athibooma@gmail.com>
2025-11-30 14:15:02 +0530

    comment lines added

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
+//this is a simple Java program that prints "Hello, World!" to the console.
+//first program in Java
 public class hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
